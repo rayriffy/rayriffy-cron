015763d671164e956707579e59bb5c536164e651
Phumrapee Limpianchop <git@rayriffy.com>
2020-12-29 15:25:35 +0000

    fix: excess space from reporter

--- apps/maimai-tracker/utils/reporter.ts
+++ apps/maimai-tracker/utils/reporter.ts
@@ -1,10 +1,9 @@
 import { chalk } from './chalk'
 
 export const reporter = {
-  info: (msg?: string) => console.log(`${chalk.black.blue('info ')}  - ${msg}`),
+  info: (msg?: string) => console.log(`${chalk.black.blue('info')}  - ${msg}`),
   warn: (msg?: string) =>
-    console.log(`${chalk.black.yellow('warn ')}  - ${msg}`),
-  fail: (msg?: string) => console.log(`${chalk.black.red('fail ')}  - ${msg}`),
-  done: (msg?: string) =>
-    console.log(`${chalk.black.green('done ')}  - ${msg}`),
+    console.log(`${chalk.black.yellow('warn')}  - ${msg}`),
+  fail: (msg?: string) => console.log(`${chalk.black.red('fail')}  - ${msg}`),
+  done: (msg?: string) => console.log(`${chalk.black.green('done')}  - ${msg}`),
 }
