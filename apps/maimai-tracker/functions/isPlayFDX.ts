e94f78cd124d4349cc6d15a462b033c8098cdf08
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-06 15:19:45 +0700

    fix: isPlayFDX

--- apps/maimai-tracker/functions/isPlayFDX.ts
+++ apps/maimai-tracker/functions/isPlayFDX.ts
@@ -1,10 +1,10 @@
+import { imageFlagCompare } from './imageFlagCompare'
+
 export const isPlayFDX = (imageFlags: (string | null)[]): boolean => {
   const acceptUrls = [
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_fsdp.png',
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_fsd.png',
   ]
 
-  return !(
-    acceptUrls.map(url => imageFlags.includes(url ?? '')).filter(o => o === true).length === 0
-  )
+  return imageFlagCompare(imageFlags, acceptUrls)
 }
