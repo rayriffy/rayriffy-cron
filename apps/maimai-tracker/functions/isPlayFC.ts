2374100c03fbfc510e07be74089a6aa5eb169d89
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-06 14:39:29 +0700

    fix: isPlayFC

--- apps/maimai-tracker/functions/isPlayFC.ts
+++ apps/maimai-tracker/functions/isPlayFC.ts
@@ -1,5 +1,5 @@
 export const isPlayFC = (imageFlags: (string | null)[]): boolean => {
-  const acceptUrl = [
+  const acceptUrls = [
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_app.png',
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_ap.png',
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_fcp.png',
@@ -7,10 +7,6 @@ export const isPlayFC = (imageFlags: (string | null)[]): boolean => {
   ]
 
   return !(
-    imageFlags
-      .map(flag => {
-        return acceptUrl.includes(flag ?? '')
-      })
-      .filter(o => o === true).length === 0
+    acceptUrls.map(url => imageFlags.includes(url ?? '')).filter(o => o === true).length === 0
   )
 }
