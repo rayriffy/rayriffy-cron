647c71a51d5dfc9e0215cd2fefd06495770a5f65
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-06 15:08:30 +0700

    chore: staged changes

--- apps/maimai-tracker/functions/isPlaySSS.ts
+++ apps/maimai-tracker/functions/isPlaySSS.ts
@@ -3,6 +3,8 @@ export const isPlaySSS = (imageFlags: (string | null)[]): boolean => {
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_sssp.png',
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_sss.png',
   ]
+  
+  console.log(imageFlags)
 
   return !(
     acceptUrls.map(url => imageFlags.includes(url ?? '')).filter(o => o === true).length === 0
