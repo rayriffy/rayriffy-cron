4afbc105707fbf70bdc7ad0e6a87de6bb953a594
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-06 15:18:34 +0700

    fix: isPlaySSS

--- apps/maimai-tracker/functions/isPlaySSS.ts
+++ apps/maimai-tracker/functions/isPlaySSS.ts
@@ -1,12 +1,10 @@
+import { imageFlagCompare } from './imageFlagCompare'
+
 export const isPlaySSS = (imageFlags: (string | null)[]): boolean => {
   const acceptUrls = [
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_sssp.png',
     'https://maimaidx-eng.com/maimai-mobile/img/music_icon_sss.png',
   ]
-  
-  console.log(imageFlags)
 
-  return !(
-    acceptUrls.map(url => imageFlags.includes(url ?? '')).filter(o => o === true).length === 0
-  )
+  return imageFlagCompare(imageFlags, acceptUrls)
 }
